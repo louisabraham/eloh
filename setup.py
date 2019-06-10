6fb47d1a6394187ed8ee35f971a30e964e6d2d02
louisabraham <louis.abraham@yahoo.fr>
2019-06-10 03:44:35 +0200

    v0.0.1

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 
 setup(
     name="eloh",
-    version="0.0.0",
+    version="0.0.1",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
     description="Send files via hole-punching",
     python_requires=">=3.6",
+    packages=["eloh"],
     entry_points={
         "console_scripts": [
             "eloh-server = eloh.server:main",
